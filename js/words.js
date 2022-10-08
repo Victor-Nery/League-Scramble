c931cd3ad358521362d8571b8b5064d5c064e89d
Victor-Nery <victor.nery2012@bol.com.br>
2022-10-08 10:40:32 -0300

    league-scramble

---
+++ js/words.js
@@ -0,0 +1,23 @@
+let words = [
+    {
+        word: "ignite",
+        hint: "Burn yours enemy"
+    },
+    {
+        word: "smite",
+        hint: "to take a dragon"
+    },
+    {
+        word: "flash",
+        hint: "scape spell"
+    },
+    {
+        word: "death mark",
+        hint: "kill adc "
+    },
+    {
+        word: "ultimate",
+        hint: "level 6"
+    },
+
+]
